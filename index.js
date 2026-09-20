6c3296172630d4e9f39128199d8669f8c26185be
Alexander Burdiss <aburdiss@icloud.com>
2026-09-19 21:17:44 -0400

    chore(release): v2.9.0

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+const appName = 'ScalePractice';
+
+AppRegistry.registerComponent(appName, () => App);
--- index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
-import App from './App';
-const appName = 'ScalePractice';
-
-AppRegistry.registerComponent(appName, () => App);
